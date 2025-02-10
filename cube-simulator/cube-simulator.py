5f595cf45514e77a8ef5df6ca14c97ad2e24af71
Miralem <miralem.banovi@89grad.ch>
2025-02-10 10:39:33 +0100

    Add basic cube simulator to generate and display a random dice roll

---
+++ cube-simulator/cube-simulator.py
@@ -0,0 +1,6 @@
+import random                                   # Bibliothek für Zufallszahlen
+
+# Eine Zufallszahl zwischen 1 und 6 generieren
+zahl = random.randint(1, 6)
+
+print("Du hast gewürfelt:", zahl)
