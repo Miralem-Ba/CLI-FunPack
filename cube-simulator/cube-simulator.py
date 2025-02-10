56e4bdfc7472b30b1ebfac6599851822f7ac8779
Miralem <miralem.banovi@89grad.ch>
2025-02-10 10:43:19 +0100

    Add loop for continuous dice rolls with exit option in cube simulator

--- cube-simulator/cube-simulator.py
+++ cube-simulator/cube-simulator.py
@@ -1,5 +1,14 @@
 import random                                   # Bibliothek für Zufallszahlen
 
+# Schleife, die so lange läuft, bis der Benutzer [q] eingibt
+while True:
+    input("Drücke [Enter] um zu würfeln oder [q] zum Beenden.")
+
+    # Wenn der Benutzer [q] eingibt, wird die Schleife beendet
+    if input == "q":
+        print("👋 Tschüss! Bis zum nächsten Mal.")
+        break
+
 # Eine Zufallszahl zwischen 1 und 6 generieren
 zahl = random.randint(1, 6)
 
