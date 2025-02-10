7f87b010cf1056a074fc41b972123a3eb913f3ed
Miralem <miralem.banovi@89grad.ch>
2025-02-10 11:33:55 +0100

    Add character sets for password generation in password generator

--- passwort-generator/passwort-generator.py
+++ passwort-generator/passwort-generator.py
@@ -1,2 +1,12 @@
-import random                                   # Zufallszahlen-Modul
-import string                                   # Modul für Buchstaben und Sonderzeichen
+import random                                                               # Zufallszahlen-Modul
+import string                                                               # Modul für Buchstaben und Sonderzeichen
+
+# Zeichen, die im Passwort vorkommen können
+buchstaben_klein = string.ascii_lowercase                                   # a-z
+buchstaben_groß = string.ascii_uppercase                                    # A-Z
+zahlen = string.digits                                                      # 0-9
+sonderzeichen = string.punctuation                                          # !@#$%&* etc.
+
+
+# Alle Zeichen zusammenfügen
+alle_zeichen = buchstaben_klein + buchstaben_groß + zahlen + sonderzeichen
