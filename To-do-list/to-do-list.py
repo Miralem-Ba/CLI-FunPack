8a7dffa8e21d7a31eac2ce616a8d2906ab87fb8c
Miralem <miralem.banovi@89grad.ch>
2025-02-10 10:12:37 +0100

    Add basic functionality for To-do list application

---
+++ To-do-list/to-do-list.py
@@ -0,0 +1,14 @@
+# Eine leere Liste für die Aufgaben
+todo_liste = []
+
+while True:
+    
+    print("To-Do-Liste:")
+    print("1. Aufgabe hinzufügen")
+    print("2. Aufgaben anzeigen")
+    print("3. Aufgabe entfernen")
+    print("4. Beenden")
+
+    wahl = input("Wähle eine Option (1-4): ")
+
+
