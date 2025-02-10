fafb30d8fc45cb3b7d5bc2ce6d3662b05541ace1
Miralem <miralem.banovi@89grad.ch>
2025-02-10 12:52:08 +0100

    Add initial setup for rock-paper-scissors game with options

---
+++ rock-paper-scissors/rock-paper-scissors.py
@@ -0,0 +1,4 @@
+import random                                                   # Importiert das Zufallsmodul, um zufällige Werte zu generieren
+
+# Definiert die Optionen des Spiels
+optionen = ["Stein", "Papier", "Schere"]
