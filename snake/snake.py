e1f31cae7d57cd0e179c07bb2b33dfd36f32fa0f
Miralem <miralem.banovi@89grad.ch>
2025-02-17 15:08:53 +0100

    Add red rectangle drawing to Snake game

--- snake/snake.py
+++ snake/snake.py
@@ -8,5 +8,7 @@ while not game_over:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             game_over=True
+    pygame.draw.rect(dis,(255,0,0),[200,150,10,10])
+    pygame.display.update()
 pygame.quit()
 quit()
