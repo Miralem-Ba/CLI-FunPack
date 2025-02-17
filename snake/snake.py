f5c3cee6d3a5be3aa7c2132bf1407df564b8d3bc
Miralem <miralem.banovi@89grad.ch>
2025-02-17 15:44:51 +0100

    Adjust display dimensions and reduce snake block size and speed for improved gameplay balance

--- snake/snake.py
+++ snake/snake.py
@@ -11,16 +11,16 @@ red = (213, 50, 80)
 green = (0, 255, 0)
 blue = (50, 153, 213)
  
-dis_width = 1500
-dis_height = 1500
+dis_width = 600
+dis_height = 400
  
 dis = pygame.display.set_mode((dis_width, dis_height))
 pygame.display.set_caption('Snake Game by Edureka')
  
 clock = pygame.time.Clock()
  
-snake_block = 30
-snake_speed = 55
+snake_block = 10
+snake_speed = 15
  
 font_style = pygame.font.SysFont("bahnschrift", 25)
 score_font = pygame.font.SysFont("comicsansms", 35)
