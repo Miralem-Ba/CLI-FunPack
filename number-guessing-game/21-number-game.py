482c0085ef0a06a2d4f28b39bae6e3da9379da56
Miralem <miralem.banovi@89grad.ch>
2025-02-18 10:30:25 +0100

    Add lose function to number guessing game to handle game termination

--- number-guessing-game/21-number-game.py
+++ number-guessing-game/21-number-game.py
@@ -4,3 +4,9 @@ def nearest_multiple_of_4(num):
     """Berechnet das nächste Vielfache von 4"""
     return num + (4 - num % 4) if num >= 4 else 4
 
+def lose(player):
+    """Gibt aus, wer verloren hat, und beendet das Spiel"""
+    print(f"{player} HAT VERLOREN! 😢")
+    print("Besseres Glück beim nächsten Mal!")
+    exit(0)
+
