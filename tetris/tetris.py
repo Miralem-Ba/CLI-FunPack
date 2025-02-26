b6ff4f5d5fa463da24b746121ea273a517047aa7
Miralem <miralem.banovi@89grad.ch>
2025-02-26 09:53:39 +0100

    Update Tetris game dimensions and block size; add color definitions

--- tetris/tetris.py
+++ tetris/tetris.py
@@ -4,7 +4,12 @@ import random
 pygame.init()
 
 # Spielfeldgröße
-WIDTH, HEIGHT = 300, 600
-BLOCK_SIZE = 30
+WIDTH, HEIGHT = 1300, 1600
+BLOCK_SIZE = 50
 COLUMNS, ROWS = WIDTH // BLOCK_SIZE, HEIGHT // BLOCK_SIZE
+
+# Farben
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+COLORS = [(0, 255, 255), (0, 0, 255), (255, 165, 0), (255, 255, 0), (0, 255, 0), (128, 0, 128), (255, 0, 0)]
 
