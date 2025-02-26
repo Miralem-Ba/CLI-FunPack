e4e0e4c857833351c8d0a57fdb65cdadf4ab3d32
Miralem <miralem.banovi@89grad.ch>
2025-02-26 09:57:16 +0100

    Add Minesweeper class with initialization and board setup

--- Minesweeper/Minesweeper.py
+++ Minesweeper/Minesweeper.py
@@ -1,1 +1,18 @@
+import random
+import tkinter as tk
+from tkinter import messagebox
 
+class Minesweeper:
+    def __init__(self, master, size=10, mines=10):
+        self.master = master
+        self.size = size
+        self.mines = mines
+        self.board = [[0 for _ in range(size)] for _ in range(size)]
+        self.buttons = [[None for _ in range(size)] for _ in range(size)]
+        self.revealed = [[False for _ in range(size)] for _ in range(size)]
+        
+        self.place_mines()
+        self.calculate_numbers()
+        self.create_widgets()
+    
+   
